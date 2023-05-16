282b05edb0f1f1a4b20bb5f534233690e83a3891
maryamrmz <maryam.rpour@gmail.com>
2023-05-17 00:53:13 +0330

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component";
-import SidebarPage from "./pages/sidebar/sidebarPage.component";
-import Header from "./components/header/header.components";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/header.components';
+import HomePage from './pages/homepage/homepage.component';
+import SidebarPage from './pages/sidebar/sidebarPage.component';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-    return (
-        <div className='container'>
-            <Header />
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/sidebar' component={SidebarPage} />
-            </Switch>
-        </div>
-    );
+  return (
+    <div className='container'>
+      <Header />
+      <Routes>
+        <Route path='/' element={HomePage} />
+        <Route path='/sidebar' element={SidebarPage} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
